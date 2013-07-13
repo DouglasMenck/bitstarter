93df914407ba7451873cea38a71e5a3afb0ca6ba
Douglas Menck <douglas.menck@gmail.com>
2013-07-12 23:50:01 -0400

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs  = require('fs');
+var buf = new Buffer(256);
 
 var app = express.createServer(express.logger());
 
+var bufLen = buf.write(fs.readFileSync("index.html",'UTF8'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString('utf8', 0, bufLen));
 });
 
 var port = process.env.PORT || 5000;
